2cd321928ad32a223d4bb5ce8d3c4b6edd7b7d33
nekaamunoz <nekane.munoz@gmail.com>
2025-02-19 17:59:09 +0100

    Fixed icons

--- base/urls.py
+++ base/urls.py
@@ -3,7 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.dashboard, name='dashboard'),
-    path('main/', views.main, name='main'),
     path('sign-in/', views.signin, name='sign-in'),
     path('sign-up/', views.signup, name='sign-up'),
     path('profile/', views.profile, name='profile'),
